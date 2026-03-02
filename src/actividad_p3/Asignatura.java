699dacb657e3318582e045008a023f4e5d3bafff
carlos daniel ospino <carlosdospino@unicesar.edu.co>
2026-03-02 09:43:15 -0500

    feat: agregar encapsulamiento a clase Asignatura

--- src/actividad_p3/Asignatura.java
+++ src/actividad_p3/Asignatura.java
@@ -1,7 +1,41 @@
 package actividad_p3;
 
+package actividad_p3;
+
 public class Asignatura {
     private String codigo;
     private String nombre;
     private int creditos;
+
+    
+    public Asignatura(String codigo, String nombre, int creditos) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
+
+    
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
 }
