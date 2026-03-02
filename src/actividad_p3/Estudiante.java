47cd83640fa49f21997d5c11097285ed1136b5ab
icamilohernandez <icamilohernandez@unicesar.edu.co>
2026-03-02 09:01:16 -0500

    feat: crear clase Estudiante con atributos

---
+++ src/actividad_p3/Estudiante.java
@@ -0,0 +1,9 @@
+package actividad_p3;
+
+public class Estudiante {
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int semestre;
+}
