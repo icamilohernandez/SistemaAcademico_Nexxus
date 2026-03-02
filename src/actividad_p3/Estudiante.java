7d4083fe04cae38b3af638f6f349eecb59f4ac97
icamilohernandez <icamilohernandez@unicesar.edu.co>
2026-03-02 09:03:36 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- src/actividad_p3/Estudiante.java
+++ src/actividad_p3/Estudiante.java
@@ -6,4 +6,54 @@ public class Estudiante {
     private String apellido;
     private int edad;
     private int semestre;
+
+ //Constructor
+    public Estudiante(String codigo, String nombre, String apellido, int edad, int semestre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.semestre = semestre;
+    }
+
+    //getter y setter
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
 }
