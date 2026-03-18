2fe206fc5988c6ccb5aff8d3029148621b31d163
carlos daniel ospino <carlosdospino@unicesar.edu.co>
2026-03-18 11:35:03 -0500

    feat: implementar CREATE - registrar nota

--- src/actividad_p3/Main.java
+++ src/actividad_p3/Main.java
@@ -4,11 +4,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    
-    static ArrayList<Nota> notas = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
+static ArrayList<Nota> notas = new ArrayList<>();
+
     
     public static void main(String[] args) {
         System.out.println("Sistema funcionando");
     }
+    public static void registrarNota() {
+    System.out.println("\n--- REGISTRAR NOTA ---");
+    
+    System.out.print("Código del estudiante: ");
+    String codEst = sc.nextLine();
+    
+    System.out.print("Código de la asignatura: ");
+    String codAsig = sc.nextLine();
+    
+    System.out.print("Valor de la nota: ");
+    double valor = sc.nextDouble();
+    sc.nextLine(); // limpiar el buffer
+    
+    Nota nota = new Nota(codEst, codAsig, valor);
+    notas.add(nota);
+    
+    System.out.println("Nota registrada correctamente.");
+}
 }
