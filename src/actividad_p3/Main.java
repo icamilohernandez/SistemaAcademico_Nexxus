6d891b5e806bee5b374820d095def606c0b4fa8d
carlos daniel ospino <carlosdospino@unicesar.edu.co>
2026-03-18 11:25:05 -0500

    feat: agregar Main.java a rama correcta

---
+++ src/actividad_p3/Main.java
@@ -0,0 +1,14 @@
+package actividad_p3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    
+    static ArrayList<Nota> notas = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        System.out.println("Sistema funcionando");
+    }
+}
